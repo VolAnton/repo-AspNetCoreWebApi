582dec74ee8d12ce19f540f0683fc53d65b00b51
VolAnton <_Tocuk@mail.ru>
2022-02-03 19:51:49 +0300

    Домашнее задание №7

---
+++ Lesson_7/Lesson_7/MetricsAgent/Controllers/DotNetMetricsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MetricsAgent.Features.Queries;
+using MediatR;
+
+namespace MetricsAgent.Controllers
+{
+    [Route("api/metrics/dotnet/errors-count")]
+    [ApiController]
+    public class DotNetMetricsController : ControllerBase
+    {
+        private readonly ILogger<DotNetMetricsController> _logger;
+        private readonly IMediator _mediator;
+
+        public DotNetMetricsController(ILogger<DotNetMetricsController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+            _logger.LogDebug(1, "NLog встроен в DotNetMetricsController");
+        }
+
+        [HttpGet("from/{fromTime}/to/{toTime}")]
+        public async Task<IActionResult> GetMetricsByPeriod([FromRoute] DotNetMetricGetByPeriodQuery request)
+        {
+            _logger.LogInformation($"Parameters: {request}");
+            
+            var response = await _mediator.Send(request);
+            
+            return Ok(response);
+        }
+    }
+}
---
+++ Lesson_7/Lesson_7/MetricsManager/Controllers/HddMetricsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MetricsManager.Features.Queries.Metrics;
+using MediatR;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/metrics/hdd/left")]
+    [ApiController]
+    public class HddMetricsController : ControllerBase
+    {
+        private readonly ILogger<HddMetricsController> _logger;
+        private readonly IMediator _mediator;
+
+        public HddMetricsController(ILogger<HddMetricsController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+            _logger.LogDebug(1, "NLog встроен в HddMetricsController");
+        }
+                
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+        public async Task<IActionResult> GetMetricsFromAgent([FromRoute] HddMetricGetByPeriodFromAgentQuery request)
+        {
+            _logger.LogInformation($"Hdd GetMetricsFromAgent Parameters: {request}");
+
+            var response = await _mediator.Send(request);
+
+            return Ok(response);
+        }
+               
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+        public async Task<IActionResult> GetMetricsFromAllCluster([FromRoute] HddMetricGetByPeriodQuery request)
+        {
+            _logger.LogInformation($"Hdd GetMetricsFromAllCluster Parameters: {request}");
+
+            var response = await _mediator.Send(request);
+
+            return Ok(response);
+        }
+    }
+}
